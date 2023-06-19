eff7c1cd85cbfd3834a19e857adc4e90e7636521
Jadilson Lima Ferreira <jadilson.lima13@hotmail.com>
2023-06-19 20:03:09 -0300

    Jaja

--- Cadastro/CadastroCliente.Designer.cs
+++ Cadastro/CadastroCliente.Designer.cs
@@ -424,6 +424,7 @@
             this.button2.TabIndex = 17;
             this.button2.Text = "CANCELAR";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CancelarCadastroCliente);
             // 
             // CadastroCliente
             // 
