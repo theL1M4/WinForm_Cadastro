ced329bdc8d55de53214a5b4eaf98298d4f2d192
ViniNC <viniciuscandido.eu@gmail.com>
2023-06-21 20:02:18 -0300

    frmCadastroCliente

--- Atividade ADO/frmCadastroCliente.cs
+++ Atividade ADO/frmCadastroCliente.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Atividade_ADO
 {
-    public partial class Form1 : Form
+    public partial class frmCadastroCliente : Form
     {
-        public Form1()
+        public frmCadastroCliente()
         {
             InitializeComponent();
         }
