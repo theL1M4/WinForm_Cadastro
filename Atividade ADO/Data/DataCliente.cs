27dc2b7abe3ffd40288f625795530ec6d422c8f9
ViniNC <viniciuscandido.eu@gmail.com>
2023-06-21 22:01:13 -0300

    DataCliente

--- Atividade ADO/Data/DataCliente.cs
+++ Atividade ADO/Data/DataCliente.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Atividade_ADO.Data
 {
-    internal class DataCliente
+    public class DataCliente
     {
+        readonly ConectaBD sqlConn = new ConectaBD();
+        readonly SqlCommand sqlCMD = new SqlCommand();
+
+        public void Inserir(Cliente cliente)
+        {
+            sqlConn.AbreBanco();
+            string strCMD = "insert";
+            sqlCMD.CommandText = strCMD;
+            sqlCMD.Parameters.AddWithValue("@nome", cliente.Nome);
+            sqlCMD.Parameters.AddWithValue("@usuario", cliente.Usuario);
+            sqlCMD.Parameters.AddWithValue("@telefone", cliente.Telefone);
+            sqlCMD.Parameters.AddWithValue("@email", cliente.Email);
+            sqlCMD.Parameters.AddWithValue("@data", DateTime.Now.ToString());
+            sqlCMD.ExecuteNonQuery();
+
+        }
     }
 }
