8e127575b56518df9ac3a3a9cacf5a8b3b5dc1b8
ViniNC <viniciuscandido.eu@gmail.com>
2023-06-21 21:40:27 -0300

    Confirmar prep.

--- Atividade ADO/Program.cs
+++ Atividade ADO/Program.cs
@@ -16,7 +16,7 @@ namespace Atividade_ADO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmCadastroCliente());
         }
     }
 }
