cbcacee15d02b66c9e1134ab293c74a2f008b3a7
Jadilson Lima Ferreira <jadilson.lima13@hotmail.com>
2023-06-22 19:57:06 -0300

    Certinho

--- Atividade ADO/Data/ConectaBD.cs
+++ Atividade ADO/Data/ConectaBD.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Atividade_ADO.Data
 {
     internal class ConectaBD
     {
-        string ConnctionString = @"Data Source";
+        string ConnctionString = @"Data Source=NOTEBOOK\SQLEXPRESS;" +
+            "Initial Catalog=AtividadeBD;" +
+            "User ID=aluno;" +
+            "Password='123'";
+        public SqlConnection SqlConn()
+        {
+            SqlConnection sqlConn = new SqlConnection(ConnctionString);
+            return sqlConn;
+        }
+        
+        public void AbreBanco()
+        {
+            var sqlConn = SqlConn();
+            sqlConn.Open();
+            if (sqlConn.State == System.Data.ConnectionState.Closed)
+            {
+
+            }
+        }
     }
 }
