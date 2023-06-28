4ce75ac39ffb0a60c1fcecf20bb221681580dc8f
Jadilson Lima Ferreira <jadilson.lima13@hotmail.com>
2023-06-27 21:52:28 -0300

    .

--- Atividade ADO/Data/DataCliente.cs
+++ Atividade ADO/Data/DataCliente.cs
@@ -10,14 +10,17 @@ namespace Atividade_ADO.Data
 {
     class DataCliente
     {
-        readonly ConectaBD sqlConn = new ConectaBD();
+        readonly ConectaBD oConectaBD = new ConectaBD();
         readonly SqlCommand sqlCMD = new SqlCommand();
 
         public void Inserir(Cliente cliente)
         {
-            sqlConn.AbreBanco();
+            //sqlConn.AbreBanco();
+            var conn = oConectaBD.SqlConn();
+            conn.Open();
             string strCMD = "INSERT INTO CLIENTES(Nome,Usuario,Telefone,Email,DtCadastro) VALUES(@Nome, @Usuario, @Telefone, @Email, @DtCadastro)";
             sqlCMD.CommandText = strCMD;
+            sqlCMD.Connection = conn;
             sqlCMD.Parameters.AddWithValue("@Nome", cliente.Nome);
             sqlCMD.Parameters.AddWithValue("@Usuario", cliente.Usuario);
             sqlCMD.Parameters.AddWithValue("@Telefone", cliente.Telefone);
