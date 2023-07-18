653b64820f8d998ef877295fd5c0ed01fefdcf1c
Jadilson Lima Ferreira <jadilson.lima13@hotmail.com>
2023-07-18 20:29:27 -0300

    fOI

---
+++ Atividade ADO/Data/DataProduto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_ADO.Data
+{
+    internal class DataProduto
+    {
+        readonly ConectaBD oConectaBD = new ConectaBD();
+        readonly SqlCommand sqlCMD = new SqlCommand();
+
+        public void Inserir(Produto produto)
+        {
+            //sqlConn.AbreBanco();
+            var conn = oConectaBD.SqlConn();
+            conn.Open();
+            string strCMD = "INSERT INTO PRODUTOS(NomeProd,TipoProd,UnMedidaProd,QuantidadeProd,) VALUES(@Nome, @Usuario, @Telefone, @Email)";
+            sqlCMD.CommandText = strCMD;
+            sqlCMD.Connection = conn;
+            sqlCMD.Parameters.AddWithValue("@Nome", produto.NomeProd);
+            sqlCMD.Parameters.AddWithValue("@Usuario", produto.TipoProd);
+            sqlCMD.Parameters.AddWithValue("@Telefone", produto.UnMedidaProd);
+            sqlCMD.Parameters.AddWithValue("@Email", produto.QuantidadeProd);
+            sqlCMD.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Atividade ADO/Data/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_ADO.Data
+{
+    internal class Produto
+    {
+        public int IdProd { get; set; }
+        public string NomeProd { get; set; }
+        public string TipoProd { get; set; }
+        public string UnMedidaProd { get; set; }
+        public string QuantidadeProd { get; set; }
+    }
+}
