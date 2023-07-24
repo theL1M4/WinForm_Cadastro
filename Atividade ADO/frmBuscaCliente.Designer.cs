e092ee7d68dbbf7eb0328ff036075c7c3c1c6810
ViniNC <viniciuscandido.eu@gmail.com>
2023-07-24 20:26:21 -0300

    Inserção frmBuscaCliente

---
+++ Atividade ADO/frmBuscaCliente.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Atividade_ADO
+{
+    partial class frmBuscaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.pnlBuscar = new System.Windows.Forms.Panel();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTituloBscClient = new System.Windows.Forms.Label();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.dtgClientes = new System.Windows.Forms.DataGridView();
+            this.pnlBuscar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.pnlGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(689, 30);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(104, 29);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // pnlBuscar
+            // 
+            this.pnlBuscar.Controls.Add(this.txtNome);
+            this.pnlBuscar.Controls.Add(this.lblNome);
+            this.pnlBuscar.Controls.Add(this.btnBuscar);
+            this.pnlBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlBuscar.Location = new System.Drawing.Point(1, 97);
+            this.pnlBuscar.Name = "pnlBuscar";
+            this.pnlBuscar.Size = new System.Drawing.Size(796, 66);
+            this.pnlBuscar.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(3, 31);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(680, 26);
+            this.txtNome.TabIndex = 4;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(3, 4);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTituloBscClient);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(20, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(213, 55);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblTituloBscClient
+            // 
+            this.lblTituloBscClient.AutoSize = true;
+            this.lblTituloBscClient.Location = new System.Drawing.Point(3, 14);
+            this.lblTituloBscClient.Name = "lblTituloBscClient";
+            this.lblTituloBscClient.Size = new System.Drawing.Size(158, 25);
+            this.lblTituloBscClient.TabIndex = 0;
+            this.lblTituloBscClient.Text = "Busca Cliente";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Controls.Add(this.dtgClientes);
+            this.pnlGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlGrid.Location = new System.Drawing.Point(1, 169);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(796, 277);
+            this.pnlGrid.TabIndex = 6;
+            // 
+            // dtgClientes
+            // 
+            this.dtgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgClientes.Location = new System.Drawing.Point(280, 15);
+            this.dtgClientes.Name = "dtgClientes";
+            this.dtgClientes.Size = new System.Drawing.Size(240, 150);
+            this.dtgClientes.TabIndex = 0;
+            // 
+            // frmBuscaCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlBuscar);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmBuscaCliente";
+            this.Text = "frmBuscaCliente";
+            this.pnlBuscar.ResumeLayout(false);
+            this.pnlBuscar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Panel pnlBuscar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTituloBscClient;
+        private System.Windows.Forms.Panel pnlGrid;
+        private System.Windows.Forms.DataGridView dtgClientes;
+    }
+}
